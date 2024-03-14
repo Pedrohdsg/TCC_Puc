810e3e06bff37229ee4fc66324d1444fa12a57e6
Pedrohdsg <pedrohdsg@gmail.com>
2024-03-14 16:38:14 -0300

    deploy

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from apps.usuarios.views import login, cadastro, logout
 
 urlpatterns = [
-    path('login', login, name='login'),
-    path('cadastro', cadastro, name='cadastro'),
-    path('logout', logout, name='logout'),
+    path('/login', login, name='login'),
+    path('/cadastro', cadastro, name='cadastro'),
+    path('/logout', logout, name='logout'),
 ]
